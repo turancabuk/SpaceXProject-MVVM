9cd6c79d4644216843acc49f679e1dccdc314a34
turan <turan6677@gmail.com>
2022-10-14 18:24:59 +0300

    Initial Commit

---
+++ SpaceXProject-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXProject-MVVM
+//
+//  Created by Turan Çabuk on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
